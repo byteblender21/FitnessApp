04a102ad5ba0ed5a32364162f9ed0143744b8de1
ByteBlender <byteblender21@gmail.com>
2023-04-05 17:13:11 +0200

    Rewrite workout detail

--- app/src/main/java/com/github/byteblender21/fitnessapp/ui/workouts/WorkoutDetailActivity.kt
+++ app/src/main/java/com/github/byteblender21/fitnessapp/ui/workouts/WorkoutDetailActivity.kt
@@ -1,18 +1,12 @@
 package com.github.byteblender21.fitnessapp.ui.workouts
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.github.byteblender21.fitnessapp.R
 import com.github.byteblender21.fitnessapp.databinding.ActivityWorkoutDetailBinding
+import com.google.android.material.snackbar.Snackbar
 
 class WorkoutDetailActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityWorkoutDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,19 +17,9 @@ class WorkoutDetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_workout_detail)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_workout_detail)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
